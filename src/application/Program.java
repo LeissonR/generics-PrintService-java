b3a819f32097de336fc241354c4670d0496d1183
Leisson Rodrigues <leisson@gmail.com>
2023-04-19 23:58:43 -0400

    issue to reuse methods in two kinds printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,29 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
+
+import model.services.PrintServiceString;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+	Scanner sc = new Scanner(System.in);
 	
+	PrintServiceString ps =  new PrintServiceString();
 		
+	System.out.print("How many values? ");
+	int n = sc.nextInt();
 	
+	for (int i = 0; i < n; i++) {
+		String value = sc.next();
+		ps.addValue(value);
+	}
 
+	ps.print();
+	System.out.println("First: " + ps.first());
+	
+	sc.close();
 	}
 
 }
